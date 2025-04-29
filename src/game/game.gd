95087a502bf7d9486e821d28f4edde2467624173
OutworldToast <Account.ander@hotmail.com>
2025-04-29 17:50:22 +0200

    added some basic movement and colour switching

--- src/game/game.gd
+++ src/game/game.gd
@@ -15,8 +15,24 @@ class_name Game
 
 @onready var player: Player = $Player
 @onready var line: Line = $Line
-
+@onready var viewport_size: Vector2 = get_viewport().get_visible_rect().size
 
 func _ready() -> void:
     player.colors = colors
     line.current_color = colors[1]
+
+    line.finished_movement.connect(_on_line_finished_movement)
+    line.move(Vector2(viewport_size.x, line.position.y))
+
+func _on_line_finished_movement() -> void:
+    line.current_color = colors.pick_random()
+    print("Key to press: %s" % OS.get_keycode_string(player.keys[colors.find(line.current_color)]))
+
+
+    var target_x: float
+    if line.position.x < 10:
+        target_x = viewport_size.x
+    elif line.position.x > viewport_size.x - 10:
+        target_x = 0
+
+    line.move(Vector2(target_x, line.position.y), 1.5)
--- src/line/line.gd
+++ src/line/line.gd
@@ -1,9 +1,21 @@
 extends Area2D
 class_name Line
 
+signal finished_movement
+
 @onready var sprite_material: ShaderMaterial = $Polygon2D.material
 
 var current_color: Color:
     set(value):
         current_color = value
         sprite_material.set_shader_parameter("color", value)
+
+func move(goal_position: Vector2, delay: float = 1.0) -> void:
+
+    var tween: Tween = create_tween()
+
+    tween.tween_property(self, "position", goal_position, delay
+        ).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+
+    tween.finished.connect(finished_movement.emit)
+    
